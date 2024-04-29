57ab8e8e65ff14cd9d98999fe8037b65be0ec632
ivansmaliakou <ivan.smaliakou@pluto.tv>
2024-04-30 00:47:08 +0200

    feat: fixed jump behaviour+added professor asset

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
+    private Animator animator;
     private Vector3 playerVelocity;
-    public float speed = 10f;
+    private Vector2 input;
+    public float angleSpeed = 400.0f;
+    private float currentAngledVelocity;
+    public float speed = 15;
+    public float jumpSpeedAccelerationRatio = 2;
     public float gravity = -9.8f;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 2f;
     public bool isGrounded = false;
+
+    public float currentRelativeAngleOy = 0;
 
     public Collider coll;
 
@@ -18,38 +28,57 @@ public class PlayerMotor : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         coll = GetComponent<Collider>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.isGrounded = this.controller.isGrounded;
+        this.isGrounded = this.CheckGround();
+    }
+    public bool CheckGround()
+    {
+        float _distanceToTheGround = this.coll.bounds.extents.y;
+        return Physics.Raycast(transform.position, Vector3.down, 0.1f);
     }
     public void ProcessMove(Vector2 input)
     {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x * this.speed * Time.deltaTime;
-        moveDirection.z = input.y * this.speed * Time.deltaTime;
-        this.controller.Move(transform.TransformDirection(moveDirection));
-        // gravity handler - every sek: -9.8 m/s along Oy if not on the ground;
-        if (this.isGrounded && this.playerVelocity.y < 0)
+        this.input = input;
+        // animation handler;
+        if (input.y == 0){
+            this.animator.SetBool("isMoving", false);
+        } else {
+            this.animator.SetBool("isMoving", true);
+        }
+        if (this.isGrounded && this.playerVelocity.y <= 0)
         {
             this.playerVelocity.y = -2;
+            this.animator.SetBool("isJumping", false);
         } else {
              this.playerVelocity.y += this.gravity * Time.deltaTime;
         }
         this.controller.Move(this.playerVelocity * Time.deltaTime);
-        Debug.Log(this.playerVelocity.y);
-        Debug.Log(this.isGrounded);
+
+        float currentMoveSpeed = speed;
+        if (this.animator.GetBool("isMoving") && this.animator.GetBool("isJumping")){
+            currentMoveSpeed *= this.jumpSpeedAccelerationRatio;
+        }
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.z = input.y * currentMoveSpeed * Time.deltaTime;
+        this.controller.Move(this.transform.TransformDirection(moveDirection));
+        
+        // rotation;
+        if (input.x != 0){
+            transform.Rotate(0, input.x * this.angleSpeed * Time.deltaTime, 0);
+        }
     }
 
     public void ProcessJump()
     {
         if (this.isGrounded)
         {
-            Debug.Log("ProcessJump is called");
-            // delta(v) = h/t + g*t
-            this.playerVelocity.y = Mathf.Sqrt(this.jumpHeight * -3f * this.gravity);
+            this.animator.SetBool("isJumping", true);
+            this.playerVelocity.y = Mathf.Sqrt(this.jumpHeight * -1 * this.gravity);
         }
     }
 }
